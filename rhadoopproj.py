a99ca38df8a0542ea8986d86a760fa15fb1c50d6
Shakti <44959605+ShaktiB@users.noreply.github.com>
2018-11-18 00:25:30 -0500

    Update rhadoopproj.py

--- rhadoopproj.py
+++ rhadoopproj.py
@@ -2,14 +2,20 @@ import matplotlib.pyplot as plt
 import numpy as np 
 import pandas as pd 
 
-theData = pd.read_csv("respiratory_samples.csv")
-theData = theData.rename(index=str, columns={"Unnamed: 0": "Original Data Index"})
+respData = pd.read_csv("respiratory_samples.csv")
+respData = respData.rename(index=str, columns={"Unnamed: 0": "Original Data Index"})
 
-respHome = theData.loc[theData['discharge_disposition_id'] == 1] 
+# Extracting all samples belonging to the group "Respiratory" such that they were discharged to home: dischargeID = 1 
+respHome = respData.loc[respData['discharge_disposition_id'] == 1] 
 
-respOther = theData.loc[theData['discharge_disposition_id'] != 1] 
+# Extracting all samples belonging to the group "Respiratory" such that they were NOT discharged to home 
+respOther = respData.loc[respData['discharge_disposition_id'] != 1] 
 
 
+columns = ["Original Data Index","admission_type_id","discharge_disposition_id","diag_1"]
 
+homeStats = respHome.describe()
+homeStats = homeStats.drop(columns,axis=1)
 
-
+otherStats = respOther.describe()
+otherStats = otherStats.drop(columns,axis=1)
